798271ab065b80947890dec874ab6727f8bf4da5
Bruno <bruno@distilando.com>
2023-06-11 14:25:32 +0100

    Screen separated in two parts
    Making the screen show the Location component on the left, the About on the right, and when clicking Home show none.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,61 @@
 import { useState } from "react";
 import "./App.css";
-import LocationComponent from "./Location";
-import AboutComponent from "./About";
+import LocationComponent from "./LocationComponent";
+import AboutComponent from "./AboutComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [activeLink, setActiveLink] = useState("home");
+  const [activeLink, setActiveLink] = useState(null);
 
   const handleLinkClick = (link) => {
     setActiveLink(link);
   };
 
   return (
-    <header>
-      <div className="header-container">
-        <div className="navbar-links">
-          <a
-            href="#"
-            className={`navbar-link ${activeLink === "home" ? "active" : ""}`}
-            onClick={() => handleLinkClick("home")}
-          >
-            Home
-          </a>
+    <div className="app-container">
+      <header>
+        <div className="header-container">
+          <div className="navbar-links">
+            <a
+              href="#"
+              className={`navbar-link ${activeLink === "home" ? "active" : ""}`}
+              onClick={() => handleLinkClick("home")}
+            >
+              Home
+            </a>
+          </div>
+          <div className="navbar-links">
+            <a
+              href="#"
+              className={`navbar-link ${
+                activeLink === "location" ? "active" : ""
+              }`}
+              onClick={() => handleLinkClick("location")}
+            >
+              Location
+            </a>
+          </div>
+          <div className="navbar-links">
+            <a
+              href="#"
+              className={`navbar-link ${
+                activeLink === "about" ? "active" : ""
+              }`}
+              onClick={() => handleLinkClick("about")}
+            >
+              About
+            </a>
+          </div>
         </div>
-        <div className="navbar-links">
-          <a
-            href="#"
-            className={`navbar-link ${
-              activeLink === "location" ? "active" : ""
-            }`}
-            onClick={() => handleLinkClick("location")}
-          >
-            Location
-          </a>
+      </header>
+      <div className="content-container">
+        <div className="left-content">
+          {activeLink === "location" && <LocationComponent />}
         </div>
-        <div className="navbar-links">
-          <a
-            href="#"
-            className={`navbar-link ${activeLink === "about" ? "active" : ""}`}
-            onClick={() => handleLinkClick("about")}
-          >
-            About
-          </a>
+        <div className="right-content">
+          {activeLink === "about" && <AboutComponent />}
         </div>
       </div>
-      {activeLink === "location" && <LocationComponent />}
-      {activeLink === "about" && <AboutComponent />}
-    </header>
+    </div>
   );
 }
 
