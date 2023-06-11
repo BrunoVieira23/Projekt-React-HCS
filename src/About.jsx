d337977376e944cd28e5b549a8793ba8f64df29b
Bruno <bruno@distilando.com>
2023-06-11 14:15:42 +0100

    Add Location and about Components
    Location and About components added, with bogus text for now!

---
+++ src/About.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our website! We are a team of passionate individuals
+        dedicated to providing high-quality services/products to our customers.
+      </p>
+      <p>
+        Our mission is to deliver exceptional experiences and exceed customer
+        expectations. We believe in continuous improvement and strive to
+        innovate in all aspects of our business.
+      </p>
+      <p>
+        Whether you're a new customer or a long-time supporter, we appreciate
+        your trust and loyalty. We're here to serve you and provide the best
+        solutions tailored to your needs.
+      </p>
+      <p>
+        Feel free to explore our website and learn more about our offerings. If
+        you have any questions or feedback, please don't hesitate to contact us.
+        We'd love to hear from you!
+      </p>
+      <p>Thank you for choosing us.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
 import { useState } from "react";
 import "./App.css";
+import LocationComponent from "./Location";
+import AboutComponent from "./About";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [activeLink, setActiveLink] = useState("home");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
 
   return (
     <header>
       <div className="header-container">
         <div className="navbar-links">
-          <a href="#" className="navbar-link">
+          <a
+            href="#"
+            className={`navbar-link ${activeLink === "home" ? "active" : ""}`}
+            onClick={() => handleLinkClick("home")}
+          >
             Home
           </a>
         </div>
         <div className="navbar-links">
-          <a href="#" className="navbar-link">
+          <a
+            href="#"
+            className={`navbar-link ${
+              activeLink === "location" ? "active" : ""
+            }`}
+            onClick={() => handleLinkClick("location")}
+          >
             Location
           </a>
         </div>
         <div className="navbar-links">
-          <a href="#" className="navbar-link">
+          <a
+            href="#"
+            className={`navbar-link ${activeLink === "about" ? "active" : ""}`}
+            onClick={() => handleLinkClick("about")}
+          >
             About
           </a>
         </div>
       </div>
+      {activeLink === "location" && <LocationComponent />}
+      {activeLink === "about" && <AboutComponent />}
     </header>
   );
 }
