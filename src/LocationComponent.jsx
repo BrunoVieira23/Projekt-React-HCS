bd4987a0062de2ac267ceba201166578b0a99780
Bruno <bruno@distilando.com>
2023-06-18 12:11:30 +0100

    Locations fitted
    Fitted the Locations, now needs the MapBox implementation.

--- src/LocationComponent.jsx
+++ src/LocationComponent.jsx
@@ -7,30 +7,46 @@ const LocationComponent = () => {
       <h1>Locations</h1>
       <div className="image-row">
         <div className="image-column">
-          <img src="placeholder1.jpg" alt="Placeholder 1" />
-          <p>Text 1</p>
+          <img src="./src/assets/Porto.jpg" alt="Porto" className="thumbnail" />
+          <p>Porto, Portugal</p>
         </div>
         <div className="image-column">
-          <img src="placeholder2.jpg" alt="Placeholder 2" />
-          <p>Text 2</p>
+          <img
+            src="./src/assets/PraiaCV.jpg"
+            alt="Praia"
+            className="thumbnail"
+          />
+          <p>Praia, Cabo Verde</p>
         </div>
         <div className="image-column">
-          <img src="placeholder3.jpg" alt="Placeholder 3" />
-          <p>Text 3</p>
+          <img
+            src="./src/assets/RiodeJaneiro.jpg"
+            alt="Rio de Janeiro"
+            className="thumbnail"
+          />
+          <p>Rio de Janeiro, Brasil</p>
         </div>
       </div>
       <div className="image-row">
         <div className="image-column">
-          <img src="placeholder4.jpg" alt="Placeholder 4" />
-          <p>Text 4</p>
+          <img src="./src/assets/Tokyo.jpg" alt="Tokyo" className="thumbnail" />
+          <p>Tokyo, Japan</p>
         </div>
         <div className="image-column">
-          <img src="placeholder5.jpg" alt="Placeholder 5" />
-          <p>Text 5</p>
+          <img
+            src="./src/assets/Tucson.jpg"
+            alt="Tucson"
+            className="thumbnail"
+          />
+          <p>Tucson, USA</p>
         </div>
         <div className="image-column">
-          <img src="placeholder6.jpg" alt="Placeholder 6" />
-          <p>Text 6</p>
+          <img
+            src="./src/assets/Hamburg.jpg"
+            alt="Hamburg"
+            className="thumbnail"
+          />
+          <p>Hamburg, Germany</p>
         </div>
       </div>
     </div>
